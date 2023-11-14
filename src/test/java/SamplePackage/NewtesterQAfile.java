5eed68fd09933567225b780d91110915da939561
bevara.mohanrao@gmail.com <77225451+bevara-mohanrao@users.noreply.github.com>
2023-11-14 12:44:50 -0500

    New file added and review other file

---
+++ src/test/java/SamplePackage/NewtesterQAfile.java
@@ -0,0 +1,11 @@
+package SamplePackage;
+
+public class NewtesterQAfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//This is new QA tester code file to test
+	}
+
+}
